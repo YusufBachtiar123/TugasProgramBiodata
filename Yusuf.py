87a39dc7ca77a5da6d6af90fa97105a6c583b7c1
YusufBachtiar123 <92924636+YusufBachtiar123@users.noreply.github.com>
2021-10-28 22:03:15 +0700

    Push Tugas

---
+++ Yusuf.py
@@ -0,0 +1,13 @@
+namalengkap = str(input('Enter name : '))
+namap = str(input('Enter nickname  : '))
+npm =int(input('Enter your NPM : '))
+umur=int(input('Enter your age : '))
+tempatlahir =str(input('Place of birth  : '))
+alamat = str(input('Your address : '))
+notelepon = int(input('Enter phone number : '))
+
+print('Assalamualaikum,')
+print('Let me introduce My self. My name is', namalengkap,'but you can call me',namap+'.')
+print('My NPM is',npm,'I was born in',tempatlahir,'and i am',umur,'years old.')
+print('I am very  glad if you want to invite my house in',alamat,'.So, dont forget to call me before with the number',notelepon,'.') 
+print('Thank you.')
---
+++ readme.md
@@ -0,0 +1,27 @@
+# *Langkah - Langkah Pembuatan Program Input Biodata Pada Python*
+## Input
+- Membuat variable
+sh
+fullname
+
+- membuat input pada variable 
+sh
+fullname = input()
+
+- membuat type data pada input
+<br> Type data *string/text* adalah *str*
+<br> Type data *integer/angka* adalah *int*
+```sh
+full name = str(input())
+phonenumber = int(input())
+```
+## Output
+- membuat output dengan *print*
+```sh
+print("assalamualaikum wr. wb.")
+```
+- penambahan *variable* pada *string* dengan menggunakan  ( , )
+```sh
+print("Let me introduce my self. My Name is ",fullname)
+print("my NPM is ",NPM,". i was born in ")
+```
